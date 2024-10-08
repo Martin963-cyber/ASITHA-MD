bab735e7fceffe5f639a6621c98b015057df1b91
Martin963-cyber <137264209+Martin963-cyber@users.noreply.github.com>
2024-10-08 11:23:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=rjh3WSKL#RRBbufpDxb_B-zwSO3XQS2lNygsFHWjsKCJE3KgDj9Y",
+MONGODB: process.env.MONGODB || "mongodb://mongo:ypiPXBRszwKoMcXtwNJKRFSRVEIHKAUS@junction.proxy.rlwy.net:39391",//enter mongo db url
 };
